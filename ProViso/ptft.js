ed682a6ab8b59295bfdb72c531d0a7517c0fe85c
bui2848 <bui2848@vandals.uidaho.edu>
2023-03-23 09:19:32 -0700

    ptft

---
+++ ProViso/ptft.js
@@ -0,0 +1,179 @@
+//correct answer is full-time
+//other answers are part-time
+
+/*var allQuestions = [
+  { /* empty!  },
+  {question: "How many children do you have?", choices: ["One", "Two", "More than two", "None"], correctAnswer: 4}, /* end question number 1 
+  {question: "Are you currently working?", choices: ["Part-time", "Full-time", "On-campus job", "None"], correctAnswer: 3}, /* end question number 2 
+  {question: "Are you paying yourself or taking loan?", choices: ["Loan", "I have support", " ", " "], correctAnswer: 2}, /* end question number 3 
+  {question: "What is your highschool GPA?", choices: ["4.0", "Lower than 3.0", "Lower than 2.0", "I don't want to answer"], correctAnswer: 1} /* end question number 4 
+];
+*/
+
+(function() {
+    var questions = [{
+      question: "How many children do you have?",
+      choices: ["One", "Two", "More than two", "None"],
+      correctAnswer: 3
+    }, {
+      question: "Are you currently working?",
+      choices: ["Part-time", "Full-time", "On-campus job", "None"],
+      correctAnswer: 2
+    }, {
+      question: "Are you paying yourself or taking loan?",
+      choices: ["Loan", "I have savings"],
+      correctAnswer: 1
+    }, {
+      question: "What is your highschool GPA?",
+      choices: ["4.0", "Lower than 3.0", "Lower than 2.0", "I don't want to answer"],
+      correctAnswer: 0
+    } 
+    ];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h3>Question ' + (index + 1) + ':</h3>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('There are ' + numCorrect + ' questions out of ' + questions.length + ' suggested you to take Full-time!');
+      if(numCorrect>=3)
+      { score.append('\nYou should take Full-time'); }
+      else
+      { score.append('\nYou should take Part-time'); }
+      return score;
+    }
+  })();
